b0d7f6460148432aca72cc9c481eef24a00feb10
lukashevich-vitaly <lykashevich.vitalij@gmail.com>
2021-12-07 23:29:13 +0300

    feat: create menu and add burgerbtn and menu

---
+++ js/index.js
@@ -0,0 +1,3 @@
+import slideMenu from "./menu.js";
+
+slideMenu('.header__burger-btn', '.navigation')
---
+++ js/menu.js
@@ -0,0 +1,6 @@
+const slideMenu = (openBtn, menu, classActiveMenu, closeTrigger) => {
+    const burgerBtn = document.querySelector(openBtn)
+    const navigation = document.querySelector(menu)
+};
+
+export default slideMenu;
